03966238ef64a32af8648cc5b0c68aadf07597ff
YasmeenYasser611 <yasmeenyasser611@gmail.com>
2026-02-08 19:42:20 +0200

    Initial project setup: Compose + MVVM structure + Hilt + base packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -56,4 +60,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.51")
+    kapt("com.google.dagger:hilt-compiler:2.51")
+
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
 }
---
+++ app/src/main/java/com/example/gamebrowser/GameBrowserApp.kt
@@ -0,0 +1,8 @@
+package com.example.gamebrowser
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GameBrowserApp : Application()
+
